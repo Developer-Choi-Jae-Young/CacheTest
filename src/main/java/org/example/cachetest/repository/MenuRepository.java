3b0f965134c5471ac28fba640aa51150ef46d36d
최재영 <cjy951213@naver.com>
2024-12-15 07:06:34 +0900

    main branch

--- src/main/java/org/example/cachetest/repository/MenuRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.cachetest.repository;
-
-import org.example.cachetest.entity.MenuEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuRepository extends JpaRepository<MenuEntity, Long> {
-}
--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into menu (name, type) values ('쁘링클', '치킨');
-insert into menu (name, type) values ('황금올리브유치킨', '치킨');
-insert into menu (name, type) values ('하와이안피자', '피자');
-insert into menu (name, type) values ('츄파츕스', '사탕');
-insert into menu (name, type) values ('간장치킨', '치킨');
-insert into menu (name, type) values ('까르보나라스파게티', '스파게티');
